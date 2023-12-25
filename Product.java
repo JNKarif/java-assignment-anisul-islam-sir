cd5e6d8f10924fa8069df58911335d0610350daa
unknown <JNKarif>
2023-12-25 17:06:24 +0600

    assignment 2

--- Product.java
+++ Product.java
@@ -1,8 +1,39 @@
+ //Assignment 1:
+// public class Product{
+//     public static void main(String[] args){
+//       
+//         //System.out.println("id: 101, \rtitle: iphone15,  \tprice: 1895 euros, \rcategory: phone,");
+       
+
+
+//     }
+// }
+
+// Assignment 2:
 public class Product{
     public static void main(String[] args){
-        System.out.println("id: 101, ");
-        System.out.println("title: iphone15, ");
-        System.out.println("price: 1895 euros, ");
-        System.out.println("category: phone,");
+        // variable declaration:
+        int id;
+        String title;
+        String price;
+        String description;
+        String category;
+
+        // variable initialization:
+        id = 101;
+        title = "iphone15";
+        price = "1895 euros";
+        description = "perfect product with best image quality";
+        category = "phone";
+
+
+
+        //print the date:
+        System.out.println("id: "+id+",");
+        System.out.println("title: "+title+",");
+        System.out.println("price: "+price+",");
+        System.out.println("description: "+description+",");
+        System.out.println("category: "+category);
+
     }
 }
